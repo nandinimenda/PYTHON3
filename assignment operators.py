b7c59c936a618ca7dd466046643ae2264f84520d
nandinimenda <160224691+nandinimenda@users.noreply.github.com>
2024-10-08 11:58:53 +0530

    Create assignment operators.py

---
+++ assignment operators.py
@@ -0,0 +1,15 @@
+a = int(input("enter a"))
+a += 5
+print("a = ",a)
+a -= 5
+print("a = ",a)
+a *= 5
+print("a = ",a)
+a /= 3
+print("a = ",a)
+a %= 3
+print("a = ",a)
+a //= 3
+print("a = ",a)
+a **= 5
+print("a = ",a)
